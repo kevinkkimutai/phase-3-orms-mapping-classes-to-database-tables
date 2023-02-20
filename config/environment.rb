a6ccd434451b66105d6753ffa305151af704ee2b
kevinkkimutai <kiruikevinkim@gmail.com>
2023-02-20 15:46:47 +0300

    Assignment completed

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,9 @@ Bundler.require
 require_relative '../lib/song'
 
 DB = { conn: SQLite3::Database.new("db/music.db") }
+
+Song.create_table
+
+# inserting to database
+hello = Song.new(name: "mine", album: "also")
+hello.save
